d8457f5c82b400e43a6b94dd84a503fe2f099933
blackball1998 <balckball1998@outlook.com>
2020-12-25 22:45:02 +0800

    second commit

--- springboot-es/src/test/java/com/azure/DemoApplicationTests.java
+++ springboot-es/src/test/java/com/azure/DemoApplicationTests.java
@@ -1,18 +1,42 @@
 package com.azure;
 
+import com.alibaba.fastjson.JSON;
+import com.azure.entities.User;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.MatchAllQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -23,15 +47,15 @@ class DemoApplicationTests {
 
     @Test
     void createIndex() throws IOException {
-        CreateIndexRequest request = new CreateIndexRequest("index2");
+        CreateIndexRequest request = new CreateIndexRequest("index1");
         CreateIndexResponse response = client.indices().create(request, RequestOptions.DEFAULT);
         System.out.println(response);
     }
 
     @Test
     void existIndex() throws IOException {
-        GetIndexRequest request1 = new GetIndexRequest("index2");
-        GetIndexRequest request2 = new GetIndexRequest("index3");
+        GetIndexRequest request1 = new GetIndexRequest("index1");
+        GetIndexRequest request2 = new GetIndexRequest("index2");
         Boolean response1 = client.indices().exists(request1, RequestOptions.DEFAULT);
         Boolean response2 = client.indices().exists(request2, RequestOptions.DEFAULT);
         System.out.println(response1);
@@ -40,8 +64,86 @@ class DemoApplicationTests {
 
     @Test
     void deleteIndex() throws IOException {
-        DeleteIndexRequest request = new DeleteIndexRequest("index2");
+        DeleteIndexRequest request = new DeleteIndexRequest("index1");
         AcknowledgedResponse response = client.indices().delete(request, RequestOptions.DEFAULT);
         System.out.println(response.isAcknowledged());
+    }
+
+    @Test
+    void addDocument() throws IOException {
+        User user = new User(18, "wang");
+        IndexRequest request = new IndexRequest("index1");
+        request.id("1");
+        request.timeout(TimeValue.timeValueSeconds(1));
+        request.source(JSON.toJSONString(user), XContentType.JSON);
+        IndexResponse response = client.index(request, RequestOptions.DEFAULT);
+        System.out.println(response.toString());
+        System.out.println(response.status());
+    }
+
+    @Test
+    void exitsDocument() throws IOException {
+        GetRequest request = new GetRequest("index1", "1");
+        request.fetchSourceContext(new FetchSourceContext(true));
+        request.storedFields("_none_");
+        boolean exists = client.exists(request, RequestOptions.DEFAULT);
+        System.out.println(exists);
+    }
+
+    @Test
+    void getDocument() throws IOException {
+        GetRequest request = new GetRequest("index1", "1");
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        System.out.println(response.getSourceAsString());
+        System.out.println(response);
+    }
+
+    @Test
+    void updateDocument() throws IOException {
+        UpdateRequest request = new UpdateRequest("index1", "1");
+        User user = new User(25, "zhang");
+        request.timeout("1s");
+        request.doc(JSON.toJSONString(user), XContentType.JSON);
+        UpdateResponse response = client.update(request, RequestOptions.DEFAULT);
+        System.out.println(response.status());
+    }
+
+    @Test
+    void deleteDocument() throws IOException {
+        DeleteRequest request = new DeleteRequest("index1", "1");
+        request.timeout("1s");
+        DeleteResponse response = client.delete(request, RequestOptions.DEFAULT);
+        System.out.println(response.status());
+    }
+
+    @Test
+    void bulkDocument() throws IOException {
+        BulkRequest request = new BulkRequest();
+        ArrayList<User> list = new ArrayList<>();
+        list.add(new User(24, "wang"));
+        list.add(new User(23, "li"));
+        list.add(new User(25, "zhang"));
+        request.timeout("10s");
+        for (int i = 0; i < list.size(); i++) {
+            request.add(new IndexRequest("index1").id(i + 1 + "").source(JSON.toJSONString(list.get(i)), XContentType.JSON));
+        }
+        BulkResponse response = client.bulk(request, RequestOptions.DEFAULT);
+        System.out.println(response.hasFailures());
+    }
+
+    @Test
+    void searchDocument() throws IOException {
+        SearchRequest request = new SearchRequest("index1");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        MatchAllQueryBuilder matchAllQueryBuilder = QueryBuilders.matchAllQuery();
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("name", "wang");
+        builder.query(termQueryBuilder);
+        builder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+        request.source(builder);
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        System.out.println(JSON.toJSONString(response.getHits()));
+        for (SearchHit hit : response.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
     }
 }
